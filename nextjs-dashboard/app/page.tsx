d61e5ef7a746daa6f1c25c383920ec8a383b596e
Iluze Ear <nsumarna150@gmail.com>
2024-11-23 22:27:54 +0700

    Install NextJS

---
+++ nextjs-dashboard/app/page.tsx
@@ -0,0 +1,13 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div
+  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+/>
+    </main>
+  );
+}
