931f0f011b76ccac6c1290761424b04a09b9239b
Iluze Ear <nsumarna150@gmail.com>
2024-11-23 22:53:35 +0700

    Menambahkan font ke latihan ~ nextjs-dashboard

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,13 +1,15 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div
+      {/* <div
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-/>
+/> */}
+<div className={styles.shape} />
     </main>
   );
 }
