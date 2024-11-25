bf84fa8bc23d1c681d9ef0374aa4d11608192f5d
Iluze Ear <nsumarna150@gmail.com>
2024-11-25 16:55:32 +0700

    Menambahkan sebuah link dengan Hook userouter

---
+++ my-app/app/dashboard/test/page.tsx
@@ -0,0 +1,9 @@
+export default function page () {
+    return (
+        <>  
+            <p>Hello World</p>
+            <p>Ini adalah page yang ada di dalam testing</p>
+        </>
+        
+    )
+}
