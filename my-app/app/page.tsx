9c37cbc3c7fca7cf47fc9f98df8030b5dce89cb1
Iluze Ear <nsumarna150@gmail.com>
2024-12-07 21:48:43 +0700

    Menambahkan sebuah tombol, dengan ReactJS. Fungsinya untuk menghitung seberapa banyak saya menekan sebuah tombol

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,22 @@
 'use client'
 import Link from "next/link"
-
+import { useState } from 'react';
 import { useRouter } from "next/navigation";
+
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Diklik {count} kali
+    </button>
+  );
+}
 
 export default function Home() {
   const router = useRouter()
@@ -11,6 +26,8 @@ export default function Home() {
         <strong>Hello WorlD</strong>
         <Link href="/dashboard">Dashboard</Link>
         <button type="button" onClick={() => router.push('/dashboard/test')}>Testing</button>
+        <MyButton />
+        
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
