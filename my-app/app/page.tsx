37f48827535bcbf103aec6bcbe9137958127d276
Iluze Ear <nsumarna150@gmail.com>
2024-12-07 22:33:08 +0700

    Menambahkan fungsi untuk menghitung seberapa banyak tombol ditekan, menambahkan 2 tombol, dan ketika salah satu tombol ditekan hasil dari keduanya sama

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,29 +4,35 @@ import { useState } from 'react';
 import { useRouter } from "next/navigation";
 
 
-function MyButton() {
+function MyButton({ counts, onClicks }) {
+  return (
+    <button onClick={onClicks}>
+      Diklik {counts} kali
+    </button>
+  );
+}
+
+
+
+export default function Home() {
+  const router = useRouter()
   const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
   }
 
-  return (
-    <button onClick={handleClick}>
-      Diklik {count} kali
-    </button>
-  );
-}
-
-export default function Home() {
-  const router = useRouter()
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <strong>Hello WorlD</strong>
         <Link href="/dashboard">Dashboard</Link>
         <button type="button" onClick={() => router.push('/dashboard/test')}>Testing</button>
-        <MyButton />
+
+        <h1>Penghitung yang diperbarui bersamaan</h1>
+        <MyButton counts={count} onClicks={handleClick} />
+        <MyButton counts={count} onClicks={handleClick} />
         
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
